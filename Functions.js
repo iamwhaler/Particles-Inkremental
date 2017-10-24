38cda0d0242abbc8f43a3472183dba18e995c9a4
iamwhaler <32680325+iamwhaler@users.noreply.github.com>
2017-10-24 19:56:28 +0300

    Proton, Neutron, Hydrogen, H2 fix. Added functions as a separate file

--- Functions.js
+++ Functions.js
@@ -1,74 +1,3 @@
-$('button').addClass('btn btn-default');
-
-$(document).ready(function () {
-
-window.setInterval(refreshNumbers, 200); 
-
-	
-$("#stringBut").click(function (){
-			
-		Player.Strings++;
-		if (Player.Strings > 9) { AddRandomBasicParticle(); };   		// add 1 to one with a probability
-		
-		});
-
-
-$('#protonsBut').click(function () {
-
-		if (Player.Ups >= 2 && Player.Downs >= 1) {
-  			Player.Protons++;
-  			Player.Ups = Player.Ups-2;
-  			Player.Downs = Player.Downs-1;
-  			};
-
-  		});			
-		
-
-
-$('#neutronsBut').click(function () {
-
-		if(Player.Ups >= 1 && Player.Downs >= 2) {
-		Player.Neutrons++;
-		Player.Ups--;
-		Player.Downs = Player.Downs - 2; 
-		};
-		});
-															
-
-$('#electronsBut').click(function () {
-	
-		Player.Electrons = Player.Electrons + 0.09 });
-
-
-
-$('#hydrogenBut').click(function () {
-		if (Player.Electrons >= 1 && Player.Neutrons >= 1 && Player.Protons >= 1) {
-				Player.Electrons--;
-				Player.Neutrons--;
-				Player.Protons--;
-				Player.Hydrogen++; 
-
-				};
-
-		});
-
-$('#h2But').click(function () {
-				
-		if (Player.Hydrogen >= 2) {
-				Player.Hydrogen = Player.Hydrogen - 2;
-				Player.H2 = Player.H2 + 1;
-
-				if (Player.H2 > 2) {								
-					var interval = setInterval( function () 
-						{Automation(0.5)} , 1000);
-
-					};
-				};
-
-		});
-
-});
-
 
 function Automation (Boost) {
 			
@@ -101,6 +30,10 @@ function refreshNumbers() {
 
 
 		};
+
+function boosterStrings() {
+	
+};
 
 function Incrementing (particle, increase) {
 		particle = particle + increase;
