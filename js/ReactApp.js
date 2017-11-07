cbd5322bf7d5fdf5610c3530d310bb26f7cccf6a
TheSimplyVlad <shubavladislav@gmail.com>
2017-11-07 11:40:35 -0500

    Как-то так)

---
+++ js/ReactApp.js
@@ -0,0 +1,29 @@
+var Player = React.createClass ({
+loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'js',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+    
+  }, 
+	getInitialState: function () {
+		return {data: [] };
+	},
+
+	render: function () {
+		return (
+			<div className="Player">
+			<Player data={this.state.data}>
+		</div>	
+	);
+	ReactDOM.render(
+	Player url="/api/Player" pollInterval={1000})
+	document.getElementById ('')
+
