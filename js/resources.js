c1e5c59a842b8008ebacfd01d914278fe393e952
iamwhaler <32680325+iamwhaler@users.noreply.github.com>
2018-01-24 11:25:07 +0200

    res+1

--- js/resources.js
+++ js/resources.js
@@ -9,8 +9,8 @@ const Atom = (Protons, Neutrons, Electrons) => ({
 const substractCost = (Player, Atom) => {
 
 	const Protons = Player.Protons - Atom.Cost.Protons
-	const Neutrons =
-	const Electrons = 
+	const Neutrons = Player.Neutrons - Atom.Cost.Neutrons
+	const Electrons = Player.Electrons - Atom.Cost.Electrons
 
 	return {
 		...Player,
