4b9a34e7395b091e7c47b6b4adc91aeaf86c2022
iamwhaler <32680325+iamwhaler@users.noreply.github.com>
2018-04-11 10:24:41 +0300

    first try to render

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,37 @@ import 'react-bootstrap';
 
 import './App.css';
 import './index.js';
+
 import './global.js'
 
 class App extends Component {
+	constructor(props) {
+	
+	super(props);
+	
+	this.state = {
+
+		gameSpeed: 1,
+
+		tick: 0,
+
+	}
+
+	this.tick = this.tick.bind(this);
+
+};
+
   render() {
     return (      
-        
-        <div></div>
+       <div> 
+        <div id = "resetGame">
+          <button>Reset game</button>
+        </div>
+
+        <div id = "saveGame">
+          <button>Save game</button>
+        </div>
+        </div>
     );
   }
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('save-reset'));
+
+
 registerServiceWorker();
